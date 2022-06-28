b7203690c899c9a2039fae1451cb2914a6ea1fdb
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2022-06-28 19:38:26 -0300

    fix: general Component

--- src/Game.js
+++ src/Game.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFound from './pages/NotFound/NotFound'
 import GamePage from './pages/Game/GamePage'
 import MainPage from './pages/MainPage/MainPage'
+import { Header } from './components/Header/Header'
 
 function Game() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<MainPage/>}/>
         <Route path="/game" element={<GamePage/>}/>
