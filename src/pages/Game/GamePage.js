c4d6f2a7998005ee5055405e6c5201b2718f8279
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2022-07-01 22:28:24 -0300

    feat: Game logic

--- src/pages/Game/GamePage.js
+++ src/pages/Game/GamePage.js
@@ -1,70 +1,54 @@
-import React, {useState, useContext} from 'react';
-import {RoundContext} from '../../context/providerRound'
+import React, { useState, useContext, useEffect } from 'react';
+import { RoundContext } from '../../context/providerRound'
 import { ResultPanel } from '../../components/Panel/ResultPanel'
 import  { SelectedOption } from '../../components/Game/SelectedOption'
 import  { PlayerOption } from '../../components/Game/PlayerOption'
 import './style.css'
 
-export const jugadas = [
+export const plays = [
   {
-    name: 'tijera',
+    name: 'scissors',
     image: '',
-    wins: ['papel'],
-    losses: ['piedra',]
+    wins: ['paper', 'lizard'],
+    losses: ['rock', 'spock']
   },
   {
-    name: 'tijera',
+    name: 'rock',
     image: '',
-    wins: '',
-    losses: ''
+    wins: ['scissors', 'lizard'],
+    losses: ['paper', 'spock']
   },
   { 
+    name: 'lizard',
     image: '',
-    name: 'tijera',
-    wins: '',
-    losses: ''
+    wins: ['spock', 'paper'],
+    losses: ['scissors', 'rock']
   },
   {
-    name: 'tijera',
+    name: 'spock',
     image: '',
-    wins: '',
-    losses: ''
+    wins: ['scissors', 'rock'],
+    losses: ['lizard', 'paper']
   },
   {
-    name: 'tijera',
+    name: 'paper',
     image: '',
-    wins: '',
-    losses: ''
+    wins: ['rock', 'spock'],
+    losses: ['scissors', 'lizard']
   }
 ]
 
 const GamePage = () => {
 
+  const addPointToPlayStatParam = (param) => {
+    setPlayStats({...playStats, [param]: playStats[param] + 1 });
+  };
+
   const [currentPlayerPlay, setCurrentPlayerPlay] = useState({});
+
   const [currentMachinePlay, setCurrentMachinePlay] = useState({});
 
   const {value, setValue}= useContext(RoundContext);
-
-
-  useEffect(() => {
-    try {     
-      someFunction();
-    } catch (error) {
-      setCurrentPlayerPlay('');
-      setCurrentMachinePlay('');
-    }  
-  }, []);
-
-
-  //TODO:definir un effect y llamar a handleRandomMachine. setear dependencia currentPlayerPlay
-  function handleRandomMachine(){
-    let jugada = generateRandom();
-    setCurrentPlayerPlay(jugada);
-  }
-
-  function generateRandom(){
-
-  }
 
   const [playStats, setPlayStats] = useState({
     wins: 0,
@@ -72,15 +56,57 @@ const GamePage = () => {
     losses: 0,
   });
 
-  //TODO: Otro effect que compruebe si el state de la jugada estan llenos, si esta lleno que calcule que sume un punto al que gano o perdio
-  const addPointToPlayStatParam = (param) => {
-    setPlayStats({...playStats, [param]: playStats[param] + 1 });
-  };
+  function handleRandomPlayMachine(){
+    let play = generateRandom();
+    setCurrentMachinePlay(play);
+    console.log(currentMachinePlay);
+  }
+
+  function generateRandom(){
+    let random = Math.floor(Math.random() * (4 - 0 + 1 ) + 0);
+    return plays[random];
+  }
+
+  function thereIsCompletePlay(){
+    return currentPlayerPlay.losses && currentMachinePlay.losses
+  }
+
+  function userWon(){
+    return thereIsCompletePlay() ? currentMachinePlay.losses.includes(currentPlayerPlay.name) : false;
+  }
+
+  function userLost(){
+    return thereIsCompletePlay() ? currentMachinePlay.wins.includes(currentPlayerPlay.name) : false;
+  }
+
+  //TODO:definir un effect y llamar a handleRandomMachine. setear dependencia currentPlayerPlay
+  useEffect(() => {
+      handleRandomPlayMachine();
+  }, [currentPlayerPlay]);
+
+  //TODO: Otro effect que compruebe si el state de las play estan llenos, si esta lleno que calcule que sume un punto al que gano o perdio
+  //
+  useEffect(() => {
+    if (userWon()){
+      addPointToPlayStatParam('wins');
+    }
+    else if (userLost()) {
+      addPointToPlayStatParam('losses');
+    }
+    else {
+      addPointToPlayStatParam('draws')
+    }
+
+    //add wait time?
+    setCurrentPlayerPlay('');
+    setCurrentMachinePlay('');
+
+  }, [currentMachinePlay]);
 
   return (
     <div className='container my-5 vh-100 d-flex justify-content-center flex-column '>
       <h1 className='title-game text-center'>Round {value}</h1>
-      <ResultPanel className='result-panel text-center' wins={playStats.wins} losses={playStats.losses} draws={playStats.draws}/>
+      <ResultPanel className='result-panel align-items-center d-flex justify-content-center flex-column' wins={playStats.wins} losses={playStats.losses} draws={playStats.draws}/>
       <div className='align-items-center d-flex justify-content-center flex-column text-center'>
         <div className='selected-options'>
           <SelectedOption currentPlayerPlay={currentPlayerPlay} currentMachinePlay={currentMachinePlay}/>
