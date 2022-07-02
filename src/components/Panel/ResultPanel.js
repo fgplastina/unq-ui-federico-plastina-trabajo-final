5f0bb4881f1219ccb9086bf290345aa625b4f16a
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2022-07-01 22:27:58 -0300

    fix: visual bug on Result panel

--- src/components/Panel/ResultPanel.js
+++ src/components/Panel/ResultPanel.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 export const ResultPanel = ({wins, losses, draws}) => {
   return  (
-    <div  className='container my-5 d-flex justify-content-center flex-column'>
-      <div className="result-panel d-flex justify-content-center flex-column align-items-center">
-       <div className="wins">
-         <span className="text column">{wins === 1 ? 'Win' : 'Wins'}</span>
+    <div  className='container mb-4 d-flex justify-content-center '>
+      <Row className='container text-center'>
+         <Col className='wins'>
+         <span className="text ">{wins === 1 ? 'Win: ' : 'Wins: '}</span>
          <span className="number">{wins}</span>
-       </div>
-       <div className="losses column">
-         <span className="text">{losses === 1 ? 'Loss' : 'Losses'}</span>
+        </Col>
+         <Col className='losses'>
+         <span className="text">{losses === 1 ? 'Loss: ' : 'Losses: '}</span>
          <span className="number">{losses}</span>
-       </div>
-       <div className="draws column">
-         <span className="text">{draws === 1 ? 'Draw' : 'Draws'}</span>
+        </Col>
+         <Col className='draws'>
+         <span className="text">{draws === 1 ? 'Draw: ' : 'Draws: '}</span>
          <span className="number">{draws}</span>
-       </div>
-       </div>
+        </Col>
+      </Row>
     </div>
 
   )
