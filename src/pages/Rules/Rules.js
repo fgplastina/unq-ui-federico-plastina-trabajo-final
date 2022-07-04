c79765877504a1677a7d7723ba0204f5c81c0367
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2022-07-04 19:48:46 -0300

    feat: rules page

--- src/pages/Rules/Rules.js
+++ src/pages/Rules/Rules.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup'
+import {plays} from '../Game/GamePage'
+import './style.css'
+
+function capitalizeFirstLetter(string){
+// https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function capitalizeWordsInList(array){
+  return array.map((word) => (capitalizeFirstLetter(word)))
+}
 
 export const Rules = () => {
   return (
-    <div className='container'>
+    <div className='container mt-5'>
+      <h2>Rules:</h2>
+      <ListGroup className='rules-list mt-3'variant="flush">
+        {plays.map((play) => (
+          <ListGroup.Item className='rules-item'>
+            <strong>{capitalizeFirstLetter(play.name)}</strong> wins to 
+            <strong className="rules-item-wins-text"> {capitalizeWordsInList(play.wins).join(', ')} </strong>
+            and losses to 
+            <strong className="rules-item-losses-text"> {capitalizeWordsInList(play.losses).join(', ')} </strong>
+          </ListGroup.Item>
+        ) )}
+      </ListGroup>
 
 
 
