1b846b155b4f6ca44284901496fa6966bab0e751
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2022-07-05 13:21:38 -0300

    feat: NotFound Page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
+import './style.css'
 
 const NotFound = () => {
-  return ( 
-    <div className='container mb-5'>
-    <h2>404</h2>
+  let navigate = useNavigate(); 
+  return (
+    <div className="row d-flex justify-content-center center flex-column p-5 container-not-found">
+      <div className="sub-container-not-found text-center">
+        <h3 className='mt-4 mb-4'>Page not found</h3>
+        <h2 className='mb-4'>404</h2>
+        
+          <Button className='mb-4' variant="outline-secondary" onClick={() => navigate('/')}>Go home</Button>{' '}
+      </div>
     </div>
-  )
+)
 }
 export default NotFound;
