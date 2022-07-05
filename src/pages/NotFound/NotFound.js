c5e479c3fa978d994736403685e3b9b3b0a12040
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2022-07-05 18:25:48 -0300

    fix: style on NotFound Page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -6,11 +6,10 @@ import './style.css'
 const NotFound = () => {
   let navigate = useNavigate(); 
   return (
-    <div className="row d-flex justify-content-center center flex-column p-5 container-not-found vh-50 align-items-center">
+    <div className="d-flex justify-content-center center p-5 container-not-found vh-50 align-items-center">
       <div className="sub-container-not-found text-center">
         <h3 className='mt-4 mb-4'>Page not found</h3>
         <h2 className='mb-4'>404</h2>
-        
           <Button className='mb-4' variant="outline-secondary" onClick={() => navigate('/')}>Go home</Button>{' '}
       </div>
     </div>
