082902391bd46970f1fb0794cbf75e8a99ac7917
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2022-07-05 10:35:16 -0300

    Updates

---
+++ precache-manifest.019abbc37f57c25dab2714faee219966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d57ab2b0054cc4c78576",
+    "url": "/unq-ui-federico-plastina-trabajo-final/static/css/main.83e9dc57.chunk.css"
+  },
+  {
+    "revision": "d57ab2b0054cc4c78576",
+    "url": "/unq-ui-federico-plastina-trabajo-final/static/js/main.946f4142.chunk.js"
+  },
+  {
+    "revision": "95f0217f690b87e49f6e",
+    "url": "/unq-ui-federico-plastina-trabajo-final/static/js/runtime~main.7193c23d.js"
+  },
+  {
+    "revision": "98124afda077c1607325",
+    "url": "/unq-ui-federico-plastina-trabajo-final/static/css/2.5f919194.chunk.css"
+  },
+  {
+    "revision": "98124afda077c1607325",
+    "url": "/unq-ui-federico-plastina-trabajo-final/static/js/2.b8c5967a.chunk.js"
+  },
+  {
+    "revision": "2f1869a31fe55240e9341a39651d6477",
+    "url": "/unq-ui-federico-plastina-trabajo-final/static/media/rock_f.2f1869a3.png"
+  },
+  {
+    "revision": "2f18a09a01bebc17eb2fdee6c51aca0f",
+    "url": "/unq-ui-federico-plastina-trabajo-final/static/media/spock_f.2f18a09a.png"
+  },
+  {
+    "revision": "f30712b7f0ff1ede6ef0e7ee93e42f65",
+    "url": "/unq-ui-federico-plastina-trabajo-final/index.html"
+  }
+];
